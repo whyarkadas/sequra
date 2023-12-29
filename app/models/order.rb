33878bca65554ced8bdacea0339c4dab0c7b8f28
Mehmet Oguz <mehmet.oguz@songkick.com>
2023-12-29 11:31:49 +0300

    Calculate fee for order, tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,35 @@ class Order < ApplicationRecord
   validates :merchant_reference, presence: true
 
   scope :not_disbursed, -> { where disbursement_id: nil }
+
+  after_create :set_fee
+  after_update_commit :set_fee, if: :saved_change_to_amount?
+
+  # TODO: Move these to ENV.
+  FEE_LOWER_LIMIT = 50
+  FEE_UPPER_LIMIT = 300
+  FEE_LOWER_LIMIT_PERCENTAGE = 1.00
+  FEE_MEDIAN_LIMIT_PERCENTAGE = 0.95
+  FEE_UPPER_LIMIT_PERCENTAGE = 0.85
+
+  def set_fee
+    update!(fee: calculate_fee)
+  end
+
+  #   1.00 % fee for orders with an amount strictly smaller than 50 €.
+  #   0.95 % fee for orders with an amount between 50 € and 300 €.
+  #   0.85 % fee for orders with an amount of 300 € or more.
+
+  def calculate_fee
+    if amount < FEE_LOWER_LIMIT
+      fee = amount * FEE_LOWER_LIMIT_PERCENTAGE / 100
+    else
+      if amount >= FEE_LOWER_LIMIT && amount < FEE_UPPER_LIMIT
+        fee = amount * FEE_MEDIAN_LIMIT_PERCENTAGE / 100
+      else
+        fee = amount * FEE_UPPER_LIMIT_PERCENTAGE / 100
+      end
+    end
+    fee
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:merchant) { create(:merchant) }
+  let(:order_params) {
+    {
+      merchant_reference: "treutel_schumm_fadel",
+      amount: 20.00,
+      creation_date: "2022-01-01",
+      id_key:"056d024481a9"
+    }
+  }
+
+  describe "#calculate_fee" do
+    context 'after create' do
+      it "amount less than 50" do
+        order = Order.create(order_params)
+        expect(order.fee.to_f).to eq 0.20
+      end
+      it "amount more than 50 less than 300" do
+        order_params[:amount] = 100
+        order = Order.create(order_params)
+        expect(order.fee.to_f).to eq 0.95
+      end
+      it "amount more than 300" do
+        order_params[:amount] = 1000
+        order = Order.create(order_params)
+        expect(order.fee.to_f).to eq 8.50
+      end
+    end
+
+    context 'after update' do
+      it "amount less than 50" do
+        order = Order.create(order_params)
+        expect(order.fee.to_f).to eq 0.20
+      end
+      it "amount than 50 less than 300" do
+        order_params[:amount] = 100
+        order = Order.create(order_params)
+        expect(order.fee.to_f).to eq 0.95
+      end
+      it "amount more than 300" do
+        order_params[:amount] = 1000
+        order = Order.create(order_params)
+        expect(order.fee.to_f).to eq 8.50
+      end
+    end
+  end
 end
